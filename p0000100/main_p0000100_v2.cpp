1066b53ef14366064469b12065eb2f14fdc49ad2
Nicolas Rey-Villamizar <nicolasreyv@gmail.com>
2012-04-20 00:19:26 -0500

    p100 new version using table

---
+++ p0000100/main_p0000100_v2.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+unsigned int step( unsigned int n );
+unsigned int convergence( unsigned int n );
+unsigned int cycles( unsigned int begin, unsigned int end );
+bool read( unsigned int &begin, unsigned int &end );
+
+static const int MAX_SIZE = 100000001;
+static unsigned int cycle[ MAX_SIZE ];
+
+int main ( void )
+{
+	cycle[1] = 1;
+	unsigned int input1, input2;
+	while( read( input1,input2 ) )
+		std::cout << input1 << ' ' << input2 << ' ' << cycles( input1, input2 ) << '\n';
+	return 0;
+}
+
+unsigned int step( unsigned int n )
+{
+	return n%2 ? 3*n+1 : n/2;
+}
+
+unsigned int convergence( unsigned int n )
+{
+	if( cycle[n] != 0 )
+		return cycle[n];
+
+	n = step( n );
+	return convergence( n ) + 1;
+}
+
+unsigned int cycles( unsigned int input1, unsigned int input2 )
+{
+	unsigned int begin, end;
+	begin = std::min(input1,input2);
+	end = std::max(input1,input2);
+	
+	unsigned int best = 0;
+	while( begin <= end ) 
+	{
+		if( cycle[begin] == 0 )
+			cycle[begin] = convergence( begin );
+		best = std::max( best, cycle[begin]);
+		begin++;
+	}
+	return best;
+}
+
+bool read( unsigned int &input1, unsigned int &input2 )
+{
+// 	unsigned int a, b;
+	std::cin >>  input1 >> input2;
+	
+// 	begin = std::min(a,b);
+// 	end = std::max(a,b);
+	
+	return std::cin;
+}
