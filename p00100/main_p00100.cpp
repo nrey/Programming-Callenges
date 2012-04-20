bcd6b01f2dcb64c2bb9887073961c0636aba38e2
Nicolas Rey-Villamizar <nicolasreyv@gmail.com>
2012-04-19 22:28:00 -0500

    test

--- p00100/main_p00100.cpp
+++ p00100/main_p00100.cpp
@@ -8,10 +8,10 @@ bool read( unsigned int &begin, unsigned int &end );
 
 int main ( void )
 {
-	unsigned int begin, end;
-	while( read( begin,end ) )
-		std::cout << begin << ' ' << end << ' ' << cycles( begin, end ) << '\n';
-	return 1;
+	unsigned int input1, input2;
+	while( read( input1,input2 ) )
+		std::cout << input1 << ' ' << input2 << ' ' << cycles( input1, input2 ) << '\n';
+	return 0;
 }
 
 unsigned int step( unsigned int n )
@@ -26,20 +26,24 @@ unsigned int convergence( unsigned int n )
 	return count;
 }
 
-unsigned int cycles( unsigned int begin, unsigned int end )
+unsigned int cycles( unsigned int input1, unsigned int input2 )
 {
+	unsigned int begin, end;
+	begin = std::min(input1,input2);
+	end = std::max(input1,input2);
+	
 	unsigned int best = 0;
 	while( begin <= end ) best = std::max( best, convergence( begin++ ) );
 	return best;
 }
 
-bool read( unsigned int &begin, unsigned int &end )
+bool read( unsigned int &input1, unsigned int &input2 )
 {
-	unsigned int a, b;
-	std::cin >>  a >> b;
+// 	unsigned int a, b;
+	std::cin >>  input1 >> input2;
 	
-	begin = std::min(a,b);
-	end = std::max(a,b);
+// 	begin = std::min(a,b);
+// 	end = std::max(a,b);
 	
 	return std::cin;
 }
